6c8f4475dd4573280a49fc6a638c4b608ccfd56c
varadhanbnm61 <varadhanbnm61@gmail.com>
2023-11-20 10:52:09 -0800

    dom2

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 
+//! session Task:-
+//? creating elements
+const div = document.createElement("div");
+const header = document.createElement("header");
+const section = document.createElement("section");
+const h1 = document.createElement("h1");
+const span = document.createElement("span");
+const article = document.createElement("article");
+const p = document.createElement("p");
+//? Adding Content
+h1.innerText="this is heading tag";
+span.innerText="Hi is this dynamic span";
+p.innerText="Hi is this dynamic paragraph";
+//?Adding class and id for styles
+div.className="container";
+header.setAttribute=("style","background-color:black")
+h1.setAttribute("class","heading")
+section.setAttribute("style","background-color:black")
+article.setAttribute("style","background-color:black")
+section.classList.add("section","section1")
+//? Appending elements
+header.appendChild(h1);
+section.appendChild(span);
+article.appendChild(p);
+div.append(header,section,article);
+document.body.append(div);
 
-const divison=document.createElement("div")
-const header=document.createElement("header")
-header.innerHTML="<h1>this is heading</h1>";
-divison.append(header);
-document.body.append(divison);
-const section=document.createElement("section");
-section.innerHTML="<span>this is span</span>";
-divison.append(section);
-const article=document.createElement("article");
-article.innerHTML="<p>this is paragraph</p>";
-divison.append(article);
 
 
 
