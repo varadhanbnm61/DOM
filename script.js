778a507570a0cacb39d735b2c43183a8787ac09c
varadhanbnm61 <varadhanbnm61@gmail.com>
2023-11-20 09:34:46 -0800

    DOM

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+const divison=document.createElement("div");
+const span=document.createElement("span");
+const header=document.createElement("header");
+header.innerHTML="hi this is header"
+span.innerHTML="hi is this dynamic span";
+divison.append(header)
+divison.append(span);
+document.body.append(divison);
+
+
+
+
+ 
+
+
+
